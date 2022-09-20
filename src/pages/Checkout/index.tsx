1375e084dcc9f2a0e5b3d2f16a6b468e7bd9c547
Gabriel Cesarino <61214104+gabrielCesarino@users.noreply.github.com>
2022-09-19 22:23:11 -0300

    style: checkout page setup

--- src/pages/Checkout/index.tsx
+++ src/pages/Checkout/index.tsx
@@ -1,9 +1,68 @@
-import { CheckoutContainer } from './styles'
+import { CurrencyDollar, MapPinLine } from 'phosphor-react'
+import {
+  CartConfirmation,
+  CheckoutContainer,
+  CustomerInformationContainer,
+  FormContainer,
+  HeaderFormContainer,
+  InputGridContainer,
+  PaymentContainer,
+} from './styles'
 
 export function Checkout() {
   return (
     <CheckoutContainer>
-      <p>Hello</p>
+      <CustomerInformationContainer>
+        <h3>Complete seu pedido</h3>
+        <FormContainer>
+          <HeaderFormContainer>
+            <span>
+              <MapPinLine size={22} color="#C47F17" />
+            </span>
+            <div>
+              <h4>Endereço de Entrega</h4>
+              <p>Informe o endereço onde deseja receber o seu pedido</p>
+            </div>
+          </HeaderFormContainer>
+          <input type="text" placeholder="CEP" />
+          <input type="text" placeholder="Rua" />
+          <InputGridContainer>
+            <input type="number" placeholder="Número" />
+            <input type="text" placeholder="Complemento" />
+          </InputGridContainer>
+          <InputGridContainer>
+            <input type="text" placeholder="Bairro" />
+            <input type="text" placeholder="Cidade" />
+            <input type="text" placeholder="UF" />
+          </InputGridContainer>
+        </FormContainer>
+        <FormContainer>
+          <HeaderFormContainer>
+            <span>
+              <CurrencyDollar size={22} color="#8047F8" />
+            </span>
+            <div>
+              <h4>Pagamento</h4>
+              <p>
+                O pagamento é feito na entrega. Escolha a forma que deseja pagar
+              </p>
+            </div>
+          </HeaderFormContainer>
+          <InputGridContainer>
+            <form>
+              <input type="radio" id="cartaoCredito" name="paymentMethod" />
+              <label htmlFor="cartaoCredito">Cartão de Crédito</label>
+              <input type="radio" id="cartaoDebito" name="paymentMethod" />
+              <label htmlFor="cartaoDebito">Cartão de Débito</label>
+              <input type="radio" id="dinheiro" name="paymentMethod" />
+              <label htmlFor="dinheiro">Dinheiro</label>
+            </form>
+          </InputGridContainer>
+        </FormContainer>
+      </CustomerInformationContainer>
+      <CartConfirmation>
+        <h3>Cafés selecionados</h3>
+      </CartConfirmation>
     </CheckoutContainer>
   )
 }
--- src/pages/Checkout/styles.ts
+++ src/pages/Checkout/styles.ts
@@ -1,3 +1,29 @@
 import styled from 'styled-components'
 
-export const CheckoutContainer = styled.div``
+export const CheckoutContainer = styled.main`
+  display: grid;
+  grid-template-columns: 40rem 28rem;
+  justify-content: space-between;
+`
+
+export const CustomerInformationContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+
+export const FormContainer = styled.form`
+  display: flex;
+  flex-direction: column;
+`
+
+export const HeaderFormContainer = styled.div`
+  display: flex;
+`
+
+export const InputGridContainer = styled.div`
+  display: flex;
+`
+
+export const PaymentContainer = styled.form``
+
+export const CartConfirmation = styled.div``
